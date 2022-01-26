59ae622b8bf913d71e25892e16ba90a23f5ffaeb
Turkie25 <91883547+Turkie25@users.noreply.github.com>
2022-01-26 20:20:27 +0100

    Add files via upload

---
+++ phpdatabase.php
@@ -0,0 +1,24 @@
+<?php 
+
+extract($_REQUEST);
+$file = fopen("form-save2.txt","a");
+
+date_default_timezone_set('Europe/Amsterdam');
+$current_date = date('d-m-Y : H:i:s');
+echo $current_date;
+
+fwrite($file, "-------------------------------\n");
+fwrite($file, "Datum : ");
+fwrite($file, $current_date ."\n");
+fwrite($file, "Naam : ");
+fwrite($file, $name ."\n");
+fwrite($file, "Email : ");
+fwrite($file, $email . "\n");
+fwrite($file, "Message : ");
+fwrite($file, $message ."\n");
+fwrite($file, "-------------------------------\n\n");
+
+fclose($file);
+header("location: http://localhost/Website/Turkie25/");
+
+?>
