6d6697b792c6ea120ca4a91e84c4a24c0e0270ef
Turkie25 <91883547+Turkie25@users.noreply.github.com>
2023-03-22 18:56:57 +0100

    portfolie website change

---
+++ app.js
@@ -0,0 +1,13 @@
+function scrollToSection() {
+  let section = document.getElementById("Section2");
+  section.scrollIntoView({ behavior: "smooth" });
+}
+
+function scrollToSection3() {
+  let section = document.getElementById("Section3");
+  section.scrollIntoView({ behavior: "smooth" });
+}
+function scrollToSection4() {
+  let section = document.getElementById("Section4");
+  section.scrollIntoView({ behavior: "smooth" });
+}
